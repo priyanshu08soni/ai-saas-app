063f876ed8b5f3558c333ad313df7a126361436c
PRIYANSHU SONI <122470603+priyanshu08soni@users.noreply.github.com>
2024-11-10 11:08:46 +0530

    commit

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -1,33 +1,32 @@
-import mongoose , {Mongoose} from "mongoose";
+import mongoose, { Mongoose } from 'mongoose';
 
 const MONGODB_URL = process.env.MONGODB_URL;
 
-interface MongooseConnection{
-    conn: Mongoose | null;
-    promise: Promise<Mongoose> | null;
+interface MongooseConnection {
+  conn: Mongoose | null;
+  promise: Promise<Mongoose> | null;
 }
 
-let cached: MongooseConnection = (global as any).mongoose;
+let cached: MongooseConnection = (global as any).mongoose
 
-if(!cached){
-    cached = (global as any).mongoose = {
-        conn: null,
-        promise:null
-    }
+if(!cached) {
+  cached = (global as any).mongoose = { 
+    conn: null, promise: null 
+  }
 }
 
-export const connectToDatabase = async()=>{
-    if(cached.conn) return cached.conn;
+export const connectToDatabase = async () => {
+  if(cached.conn) return cached.conn;
 
-    if(!MONGODB_URL) throw new Error('Missing MONGODB_URL');
-    cached.promise = 
-        cached.promise || 
-        mongoose.connect(
-            MONGODB_URL, 
-            { 
-                dbName: 'megaminds', bufferCommands:false
-            }
-        );
-    cached.conn = await cached.promise;
-    return cached.conn;
+  if(!MONGODB_URL) throw new Error('Missing MONGODB_URL');
+
+  cached.promise = 
+    cached.promise || 
+    mongoose.connect(MONGODB_URL, { 
+      dbName: 'MegaMinds', bufferCommands: false 
+    })
+
+  cached.conn = await cached.promise;
+
+  return cached.conn;
 }
