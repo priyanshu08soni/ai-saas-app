697f430229ef5dde9dee1fa24a5da4a297b914fa
PRIYANSHU SONI <122470603+priyanshu08soni@users.noreply.github.com>
2024-11-10 11:22:12 +0530

    commit

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -1,32 +1,35 @@
 import mongoose, { Mongoose } from 'mongoose';
 
 const MONGODB_URL = process.env.MONGODB_URL;
-
 interface MongooseConnection {
   conn: Mongoose | null;
   promise: Promise<Mongoose> | null;
 }
 
-let cached: MongooseConnection = (global as any).mongoose
+// Extend the global object with mongoose type
+declare global {
+  // `globalThis` is used here to access the global scope in TypeScript
+  var mongoose: MongooseConnection | undefined;
+}
 
-if(!cached) {
-  cached = (global as any).mongoose = { 
-    conn: null, promise: null 
-  }
+let cached: MongooseConnection = global.mongoose || { conn: null, promise: null };
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
 }
 
 export const connectToDatabase = async () => {
-  if(cached.conn) return cached.conn;
+  if (cached.conn) return cached.conn;
 
-  if(!MONGODB_URL) throw new Error('Missing MONGODB_URL');
+  if (!MONGODB_URL) throw new Error('Missing MONGODB_URL');
 
   cached.promise = 
     cached.promise || 
     mongoose.connect(MONGODB_URL, { 
       dbName: 'MegaMinds', bufferCommands: false 
-    })
+    });
 
   cached.conn = await cached.promise;
 
   return cached.conn;
-}
+};
