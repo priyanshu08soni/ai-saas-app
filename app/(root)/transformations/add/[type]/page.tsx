b3689b6f70b4e2132062477c59fc8e835008b8b5
PRIYANSHU SONI <122470603+priyanshu08soni@users.noreply.github.com>
2024-11-13 19:46:10 +0530

    commit

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -6,14 +6,8 @@ import { auth } from '@clerk/nextjs/server';
 import { getUserById } from '@/lib/actions/user.action';
 import { redirect } from 'next/navigation';
 
-type TransformationTypePageProps = {
-  params: {
-    type: "restore" | "fill" | "remove" | "recolor" | "removeBackground";
-  };
-};
-const AddTransformationTypePage = async({
-  params: { type }
-}:TransformationTypePageProps) => {
+
+const AddTransformationTypePage = async({ params: {type} } : SearchParamProps) => {
   const { userId }  = await auth();
   const transformation = transformationTypes[type];
   if(!userId) redirect('/sign-in');
